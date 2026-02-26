299cd1168e500de3d61e8922da6896aa73230af6
fandi <alfandi.safira@iconpln.co.id>
2026-02-26 12:44:34 +0700

    feat: implement send file excel by wa

--- src/excel/excel.controller.ts
+++ src/excel/excel.controller.ts
@@ -1,14 +1,17 @@
 // excel.controller.ts
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { ExcelService } from './excel.service';
 import { MahasiswaService } from 'src/mahasiswa/mahasiswa.service';
+import { WhatsappService } from 'src/whatsapp/whatsapp.service';
+import * as fs from 'fs';
 
 @Controller('api/excel')
 export class ExcelController {
   constructor(
     private readonly excelService: ExcelService,
-    private readonly mahasiswaService: MahasiswaService
+    private readonly mahasiswaService: MahasiswaService,
+    private readonly whatsappService: WhatsappService,
 ) {}
 
   @Get('export')
@@ -18,5 +21,41 @@ export class ExcelController {
     const { data } = response;
 
     await this.excelService.exportToExcel(data, res);
+  }
+
+  @Get('wa-status')
+  getWaStatus() {
+    return this.whatsappService.getStatus();
+  }
+
+  @Post('send-wa')
+  async sendToWhatsapp(@Body() body: { phone: string }) {
+    const status = this.whatsappService.getStatus();
+
+    if (!status.is_ready) {
+      return {
+        success: false,
+        need_scan: true,
+        qr: status.qr,
+      };
+    }
+
+    const response: { success : boolean; data: any[]; message: string } = await this.mahasiswaService.getMahasiswa();
+
+    const { data } = response;
+
+    const filePath = await this.excelService.generateExcelFile(data, 'data_mahasiswa.xlsx');
+
+    await this.whatsappService.sendExcel(body.phone, filePath, '📊 Berikut Laporan Data Mahasiswa');
+
+    fs.unlinkSync(filePath);
+
+    return { message: 'File berhasil dikirim ke WhatsApp!' };
+  }
+
+  @Post('wa-logout')
+  async logout() {
+    await this.whatsappService.logout();
+    return { message: 'Berhasil logout dari WhatsApp' };
   }
 }
--- src/excel/excel.service.ts
+++ src/excel/excel.service.ts
@@ -7,6 +7,35 @@ import * as fs from 'fs';
 
 @Injectable()
 export class ExcelService {
+  async generateExcelFile(data: any[], filename = 'data_mahasiswa.xlsx'): Promise<string> {
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet('Data_Mahasiswa');
+
+    sheet.columns = [
+      { header: 'NIM', key: 'nim', width: 20},
+      { header: 'Nama', key: 'nama_mahasiswa', width: 20 },
+      { header: 'Jenis Kelamin', key: 'jenis_kelamin', width: 20 },
+      { header: 'Kode Jurusan', key: 'jurusan', width: 20 },
+      { header: 'IPK', key: 'ipk', width: 10, style: { alignment: { horizontal: 'right' } } },
+    ];
+
+    sheet.getRow(1).font = { bold: true };
+
+    sheet.getRow(1).eachCell((cell) => {
+        cell.alignment = { horizontal: 'left' }
+    })
+
+    sheet.addRows(data);
+
+    const dir = path.join(process.cwd(), 'tmp');
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+    const filePath = path.join(dir, filename);
+    await workbook.xlsx.writeFile(filePath);
+
+    return filePath;
+  }
+
   async exportToExcel(data: any[], res: Response): Promise<void> {
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet('Data_Mahasiswa');
